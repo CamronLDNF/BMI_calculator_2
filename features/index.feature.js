237ae6fdcaffdfe540c8e382c3c67939d3ff435a
camron <camronmoradi@gmail.com>
2018-12-20 14:30:22 +0700

    Updates index page to include unit selector, makes test pass

--- features/index.feature.js
+++ features/index.feature.js
@@ -16,13 +16,13 @@ describe('User can calculate BMI', () => {
     await browser.close();
   });
 
-  context('by selecting Metric input', () => {
+  context('by selecting Metric method', () => {
 
     beforeEach(async () => {
       await browser.selectOption("select[id='unit']", {option: 'Metric' })
     });
     
-    it('and inputing his weight and height in metric', async () => {
+    it('and inputing his weight and height in metric units', async () => {
 
       await browser.fillIn("input[id='weight-in-kgs']", { with: "95" })
       await browser.fillIn("input[id='height-in-cm']", { with: "185" })
@@ -32,5 +32,22 @@ describe('User can calculate BMI', () => {
       expect(content).to.eql('Your BMI is 27.76');
     });
   });
-  
+
+  // context('by selecting Imperial method', () => {
+
+  //   beforeEach(async () => {
+  //     await browser.selectOption("select[id='unit']", {option: 'Imperial' })
+  //   });
+    
+  //   it('and inputing his weight and height in imperial units', async () => {
+
+  //     await browser.fillIn("input[id='weight-in-lbs']", { with: "170" })
+  //     await browser.fillIn("input[id='height-in-in']", { with: "67" })
+
+  //     await browser.clickOnButton("button")
+  //     let content = await browser.getContent("span[id='display_value']")
+  //     expect(content).to.eql('Your BMI is 27.76');
+  //   });
+  // });
+
 });
