7bc97a17064a5f5fda76d5475edaa428111cc23b
camron <camronmoradi@gmail.com>
2018-12-20 15:42:41 +0700

    Revises features tests

--- features/index.feature.js
+++ features/index.feature.js
@@ -15,39 +15,46 @@ describe('User can calculate BMI', () => {
   after(async () => {
     await browser.close();
   });
-
-  context('by selecting Metric method', () => {
-
-    beforeEach(async () => {
-      await browser.selectOption("select[id='unit']", {option: 'Metric' })
-    });
     
-    it('and inputing his weight and height in metric units', async () => {
+  it('by using Metric method', async () => {
 
-      await browser.fillIn("input[id='weight-in-kgs']", { with: "95" })
-      await browser.fillIn("input[id='height-in-cm']", { with: "185" })
+    await browser.fillIn("input[id='weight']", { with: "95" });
+    await browser.fillIn("input[id='height']", { with: "185" });
 
-      await browser.clickOnButton("button")
-      let content = await browser.getContent("span[id='display_value']")
-      expect(content).to.eql('Your BMI is 27.76');
-    });
+    await browser.selectOption("select[id='method']", {option: 'Metric' })
+
+    await browser.clickOnButton("button")
+    let content = await browser.getContent("span[id='display_value']")
+    expect(content).to.eql('Your BMI is 27.76');
   });
 
-  context('by selecting Imperial method', () => {
+  it('by using Imperial method', async () => {
 
-    beforeEach(async () => {
-      await browser.selectOption("select[id='unit']", {option: 'Imperial' })
-    });
-    
-    it('and inputing his weight and height in imperial units', async () => {
+    await browser.fillIn("input[id='weight']", { with: "170" });
+    await browser.fillIn("input[id='height']", { with: "67" });
 
-      await browser.fillIn("input[id='weight-in-lbs']", { with: "170" })
-      await browser.fillIn("input[id='height-in-in']", { with: "67" })
+    await browser.selectOption("select[id='method']", {option: 'Imperial' })
 
-      await browser.clickOnButton("button")
-      let content = await browser.getContent("span[id='display_value']")
-      expect(content).to.eql('Your BMI is 26.33');
-    });
+    await browser.clickOnButton("button")
+    let content = await browser.getContent("span[id='display_value']")
+    expect(content).to.eql('Your BMI is 26.62');
   });
+
+  // context('by selecting Imperial method', () => {
+
+  //   beforeEach(async () => {
+  //     await browser.selectOption("select[id='unit']", {option: 'Imperial' })
+  //   });
+    
+  //   it('and inputing his weight and height in imperial units', async () => {
+
+  //     await browser.fillIn("input[id='weight-in-lbs']", { with: "170" })
+  //     await browser.fillIn("input[id='height-in-in']", { with: "67" })
+
+  //     await browser.clickOnButton("button")
+  //     let content = await browser.getContent("span[id='display_value']")
+  //     expect(content).to.eql('Your BMI is 26.33');
+  //   });
+  // });
 
 });
